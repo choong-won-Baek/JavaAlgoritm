67101ce8eef61744a997ff7d837f6c39d1254b5c
wonone <wonone0902@gmail.com>
2021-12-13 00:22:59 +0900

    fix

--- algorithm/src/algorithm/overAverage.java
+++ algorithm/src/algorithm/overAverage.java
@@ -4,29 +4,35 @@ import java.util.*;
 public class overAverage {
  public static void main(String[] args) {
 	 Scanner sc= new Scanner(System.in);
+	 int[] arr;
 	 int n = sc.nextInt();
-	 int[] a = new int[n];
-	 for(int i=0; i<n; i++) {
-		 a[i] = sc.nextInt();
+
+	 
+	 for(int j = 0; j<n; j++) {
+		 
+	 int m = sc.nextInt();
+	 arr = new int[m];
+	 double sum = 0;
+	 for(int i=0; i<m; i++) {
+		 int val = sc.nextInt();
+		 arr[i] = val;
+		 sum+=val;
 	 }
-	 int sum = 0;
-	  for(int i=0; i<n; i++) {
-		  sum+=a[i];
-	  }
-	  int avg = sum/n;
-	  int count = 0;
-	  for(int i=0; i<n; i++) {
-		 if(a[i]>avg) {
+	
+	  double avg = sum/m;
+	  double count = 0;
+	  
+	  for(int i=0; i<m; i++) {
+		 if(arr[i]>avg) {
 			 count++;
 		 }
 	  }
-	  double answer = count/(double)n;
-	  
-	  System.out.print((double)Math.round(answer*100000)/1000+"%");
+	
+	  System.out.printf("%.3f%%\n", (count/m)*100);
 	  
  }
-	
-	
+	sc.close();
+ }	
 }
 //대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
 
