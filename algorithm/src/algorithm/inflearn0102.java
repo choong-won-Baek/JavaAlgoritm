f22ebfa508f2e660d6dac6f6267c92b6afcdf0e2
wonone <wonone0902@gmail.com>
2022-02-04 21:56:23 +0900

    java algorithm

--- algorithm/src/algorithm/inflearn0102.java
+++ algorithm/src/algorithm/inflearn0102.java
@@ -1,6 +1,6 @@
 package algorithm;
 import java.util.*;
-//한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+//한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요..
 
 public class inflearn0102 {
     public static String solution(String in) {
