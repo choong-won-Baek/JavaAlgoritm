51c8175ae8f59ff64ea2350a40011e44d7b19920
wonone <wonone0902@gmail.com>
2022-02-20 19:10:59 +0900

    문제 지문 추가

--- algorithm/src/algorithm/inflearn0302.java
+++ algorithm/src/algorithm/inflearn0302.java
@@ -2,7 +2,9 @@ package algorithm;
 import java.util.*;
 
 public class inflearn0302 {
-	//오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+	//A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
+
+
 	public static ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
 		ArrayList<Integer> answer = new ArrayList<Integer>();
 	    Arrays.sort(a);
--- algorithm/src/algorithm/inflearn0305.java
+++ algorithm/src/algorithm/inflearn0305.java
@@ -1,5 +1,17 @@
 package algorithm;
 import java.util.*;
+/*N입력으로 양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는 방법의 가짓수를 출력하는 프로그램을 작성하세요.
+
+만약 N=15이면
+
+7+8=15
+
+4+5+6=15
+
+1+2+3+4+5=15
+
+
+*/
 
 public class inflearn0305 {
 	public static int solution(int n) {
